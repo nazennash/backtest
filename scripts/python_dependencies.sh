008a602340e9b148cdd59a0417dac332a0d710c2
nazennash <nazennash120@gmail.com>
2025-08-28 00:09:27 +0300

    small_change_1

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-python -m venv venv
-source venv/bin/activate
+
+# Install venv support if missing
+sudo apt install python3-venv -y
+
+# Create a venv
+python3 -m venv ~/myenv
+
+# Activate it
+source ~/myenv/bin/activate
+
+# Upgrade pip inside venv
+pip install --upgrade pip
+
+
 pip install -r /home/ubuntu/django-app-v4/requirements.txt
