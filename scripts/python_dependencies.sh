5cf89208704e86d25d1fb041b3ffa8bfcdc7e005
nazennash <nazennash120@gmail.com>
2025-08-28 00:56:38 +0300

    Update gunicorn and nginx configurations; streamline python dependencies script

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -7,21 +7,3 @@ python3 -m venv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install --upgrade pip
 pip install -r /home/ubuntu/django-app-v4/requirements.txt
-
-
-# Install venv support if missing
-sudo apt install python3-venv -y
-
-# If the venv doesn't exist, create it
-if [ ! -d "/home/ubuntu/env" ]; then
-    python3 -m venv /home/ubuntu/env
-fi
-
-# Activate the venv
-source /home/ubuntu/env/bin/activate
-
-# Upgrade pip inside venv
-pip install --upgrade pip
-
-# Install project dependencies
-pip install -r /home/ubuntu/django-app-v4/requirements.txt
