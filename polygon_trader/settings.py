6a654a267b5c41343a6f0c05099d52a2655352c3
nazennash <nazennash120@gmail.com>
2025-09-02 15:59:16 +0300

    small_change

--- polygon_trader/settings.py
+++ polygon_trader/settings.py
@@ -131,7 +131,7 @@ if os.getenv('REDIS_URL') or os.path.exists('redis_enabled.txt'):
     CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://nash@127.0.0.1:6379/1",
+        "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
